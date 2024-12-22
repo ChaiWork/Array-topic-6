570a7326bdce8f078a81ef4bf0ff2173e9c9c523
chairis <pchairispum@gmail.com>
2024-12-22 23:50:38 +0800

    first commit

---
+++ ARRAY_TOPIC6.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ARRAY_TOPIC6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] penPrice = {2.00, 12.00, 50.00, 30.00};
+        double[] bookPrice = new double[4];
+        double[] markerPrice = new double[4];
+        double[] totalPrice = new double[4];
+
+        for (int i = 0; i < bookPrice.length; i++) {
+            System.out.println("Enter price for book" + (i + 1) + ":");
+            bookPrice[i] = scanner.nextDouble();
+        }
+
+        for (int i = 0; i < markerPrice.length; i++) {
+            System.out.println("Enter price for marker" + (i + 1) + ":");
+            markerPrice[i] = scanner.nextDouble();
+        }
+
+        for (int i = 0; i < totalPrice.length; i++) {
+          totalPrice[i]=bookPrice[i]+markerPrice[i]+penPrice[i];
+          System.out.println("the total price for pen,book,marker at totalprice [ " +i+ " ] is RM "+totalPrice[i]);
+        }
+        scanner.close();
+    }
+}
---
+++ TraceOutputArray.java
@@ -0,0 +1,34 @@
+public class TraceOutputArray {
+
+    public static void main(String[] args) {
+        // Create an object of the class to call the non-static method
+        TraceOutputArray traceOutput = new TraceOutputArray();
+
+        // Call the method
+        traceOutput.traceOutputArray1();
+    }
+
+    // Method to demonstrate array operations
+    public void traceOutputArray1() {
+        // Initialize an array
+        int[] arrayOfNum = new int[5];
+
+        // Fill the array
+        for (int i = 0; i < arrayOfNum.length; i++) {
+            arrayOfNum[i] = 2 * i; // Populate with multiples of 2
+        }
+
+        // Print the array
+        for (int i = 0; i < arrayOfNum.length; i++) {
+            System.out.print(arrayOfNum[i] + " "); // Print elements in a single line
+        }
+    }
+
+    public void traceOutputArray2(){
+
+        double tide[] ={12.2,-7.3,14.2,11.3};
+        System.out.print
+
+        
+    }
+}
